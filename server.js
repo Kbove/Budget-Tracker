e2f29498daaf5c6bfb28623c901e6f615323d3bc
Kbove <kbove94@gmail.com>
2021-11-17 18:39:33 -0800

    deploying to heroku

--- server.js
+++ server.js
@@ -15,8 +15,10 @@ app.use(express.json());
 
 app.use(express.static("public"));
 
-mongoose.connect("mongodb://localhost/budget", {
+mongoose.connect(process.env.MONGODB_URI || "mongodb+srv://Tennis1994:Tennis1994@cluster0.29268.mongodb.net/myFirstDatabase?retryWrites=true&w=majority", {
   useNewUrlParser: true,
+  useFindAndModify: true,
+  useCreateIndex: true,
   useFindAndModify: false
 });
 
